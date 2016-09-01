de59c3ab4c129d7b574d019c9f645f048f490acf
lixd <875697126@qq.com>
2016-09-01 08:30:06 +0800

    change background image

--- app/feed/mocks.js
+++ app/feed/mocks.js
@@ -6,7 +6,8 @@ let cards = [
       picture: 'http://lorempixel.com/80/80/people/1/',
     },
     card: {
-      text: "I am working on implementing Facebook Paper like interactions today."
+        image:require('../imgs/1.jpg'),
+        text: "I am working on implementing Facebook Paper like interactions today."
     }
   },
   {
@@ -16,8 +17,8 @@ let cards = [
       picture: 'http://lorempixel.com/80/80/people/2/',
     },
     card: {
-      image: "http://i.imgur.com/iliFxTz.jpg",
-      text: "Gotta munch on some thing while I get this to work."
+        image:require('../imgs/2.jpg'),
+        text: "Gotta munch on some thing while I get this to work."
     }
   },
   {
@@ -27,7 +28,8 @@ let cards = [
       picture: 'http://lorempixel.com/80/80/people/3/',
     },
     card: {
-      text: "Is it really possible that animations can be paused in flight to keep the interface always responsive?"
+        image:require('../imgs/3.jpg'),
+        text: "Is it really possible that animations can be paused in flight to keep the interface always responsive?"
     }
   },
   {
@@ -37,7 +39,8 @@ let cards = [
       picture: 'http://lorempixel.com/80/80/people/4/',
     },
     card: {
-      text: "Gotta ship this soon!"
+        image:require('../imgs/4.jpg'),
+        text: "Gotta ship this soon!"
     }
   },
   {
@@ -47,8 +50,8 @@ let cards = [
       picture: 'http://lorempixel.com/80/80/people/5/',
     },
     card: {
-      image: "http://i.imgur.com/Ui3oFTh.jpg",
-      text: "If this works, I need a party!"
+        image:require('../imgs/5.jpg'),
+        text: "If this works, I need a party!"
     }
   },
   {
@@ -58,7 +61,8 @@ let cards = [
       picture: 'http://lorempixel.com/80/80/people/6/',
     },
     card: {
-      text: "The Animated library for React Native is pretty amazing, right?"
+        image:require('../imgs/6.jpg'),
+        text: "The Animated library for React Native is pretty amazing, right?"
     }
   },
   {
@@ -68,7 +72,7 @@ let cards = [
       picture: 'http://lorempixel.com/80/80/people/7/',
     },
     card: {
-      image: "http://i.imgur.com/HOXLJJS.jpg",
+      image: require('../imgs/7.jpg'),
       text: "Looks like there is something to eat here."
     }
   },
